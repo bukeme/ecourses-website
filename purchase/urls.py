d0f4930883e0ea2939b311abe125a9f8901db819
Ukeme <ukeme@localhost.localdomain>
2023-06-14 22:06:23 +0100

    worked on coupon functionality

--- purchase/urls.py
+++ purchase/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 	path('cart/<int:course_pk>/add/', views.cart_add_view, name='cart_add'),
 	path('cart/', views.cart_item_list_view, name='cart'),
 	path('cart/<int:course_pk>/remove/', views.cart_item_remove_view, name='cart_remove'),
+	path('coupon/apply/', views.apply_coupon, name='apply_coupon'),
+	path('coupon/remove/', views.remove_coupon, name='remove_coupon'),
 ]
